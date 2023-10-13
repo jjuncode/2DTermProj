3b93966908318be90c1a0575f4816d60cb3135a5
jjuncode <jjun.code@gmail.com>
2023-10-14 02:16:31 +0900

    Player keyupdate func

--- src/object/player.py
+++ src/object/player.py
@@ -16,18 +16,15 @@ class Player:
         self.ani_reset = False
         self.ani = Ani("Character.png", [8,12,7,7], [128,128,200,200],[120,120,210,210])
 
-    def render(self):
-        self.ani.render(self.pos)
-
-    def update(self):
-        if ( GetKey(SDLK_a) == "TAP" or GetKey(SDLK_a) ==  "HOLD"):
+    def UpdateKey(self):
+        if (GetKey(SDLK_a) == "TAP" or GetKey(SDLK_a) == "HOLD"):
             self.pos.x -= TimeMgr.GetDt() * self.speed
         if GetKey(SDLK_w) == "TAP" or GetKey(SDLK_w) == "HOLD":
             self.pos.y += TimeMgr.GetDt() * self.speed
         if GetKey(SDLK_s) == "TAP" or GetKey(SDLK_s) == "HOLD":
-            self.pos.y -=TimeMgr.GetDt() * self.speed
+            self.pos.y -= TimeMgr.GetDt() * self.speed
         if GetKey(SDLK_d) == "TAP" or GetKey(SDLK_d) == "HOLD":
-            self.pos.x +=TimeMgr.GetDt() * self.speed
+            self.pos.x += TimeMgr.GetDt() * self.speed
         if GetKey(SDLK_q) == "TAP" or GetKey(SDLK_q) == "HOLD":
             self.ani.cur_ani = 2
             self.ani_reset = True
@@ -35,7 +32,13 @@ class Player:
             self.ani.cur_ani = 3
             self.ani_reset = True
 
+    def render(self):
+        self.ani.render(self.pos)
+
+    def update(self):
+        self.UpdateKey()
         self.ani.update()
+
 
 class Idle:
 
