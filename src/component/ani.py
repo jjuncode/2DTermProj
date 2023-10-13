667ef8185060a17d78bc8e9d3ee5e40657a237a3
jjuncode <jjun.code@gmail.com>
2023-10-14 02:15:01 +0900

    Character Control w,a,s,d,e,q
    Draw animation by w,a,s,d,e,q,
    -> e,q = attack motion

--- src/component/ani.py
+++ src/component/ani.py
@@ -18,19 +18,19 @@ class Ani:
         self.act_time = 0.1 # 프레임 건너는 시간
 
         self.acc_time =0.0  # 누적시간
+        self.ani_reset =False
 
     def render(self,pos):
-        self.image.clip_draw(self.cur_frame*self.offset_x[self.cur_ani] # x
+            self.image.clip_draw(self.cur_frame*self.offset_x[self.cur_ani] # x
                              ,sum(self.offset_y[:self.cur_ani])         # y
                              ,self.offset_x[self.cur_ani],self.offset_y[self.cur_ani]    # offset
                              ,pos.x,pos.y   # pos
                              ,self.offset_x[self.cur_ani] * 4       # size_x
                              ,self.offset_y[self.cur_ani] * 4)      # size_y
 
-    def update(self,_cur_ani,_reset):
+    def update(self):
         self.acc_time += TimeMgr.GetDt()
-        self.cur_ani = _cur_ani
-        if (_reset) : self.cur_frame = 0
+        if (self.ani_reset) : self.cur_frame = 0
         if (self.acc_time > self.act_time ):
             self.acc_time = 0
             self.cur_frame = ( self.cur_frame +1 ) % self.max_frame[self.cur_ani]
--- src/object/player.py
+++ src/object/player.py
@@ -1,14 +1,13 @@
 from src.struct.struct import Pos
 from src.component.ani import Ani
 from src.mgr.KeyMgr import GetKey
-from pico2d import SDLK_a,SDLK_d,SDLK_w,SDLK_s,SDLK_1,SDLK_2,SDLK_3,SDLK_4
+from pico2d import SDLK_a,SDLK_d,SDLK_w,SDLK_s,SDLK_q,SDLK_e
 from src.mgr.TimeMgr import TimeMgr
 
 class Player:
     def __init__(self):
         self.pos = Pos()
         self.speed = 500
-        self.dir = 1
 
         self.state = StateMachine(self)
 
@@ -23,37 +22,35 @@ class Player:
     def update(self):
         if ( GetKey(SDLK_a) == "TAP" or GetKey(SDLK_a) ==  "HOLD"):
             self.pos.x -= TimeMgr.GetDt() * self.speed
-            print(TimeMgr.GetDt() * 100)
         if GetKey(SDLK_w) == "TAP" or GetKey(SDLK_w) == "HOLD":
             self.pos.y += TimeMgr.GetDt() * self.speed
         if GetKey(SDLK_s) == "TAP" or GetKey(SDLK_s) == "HOLD":
             self.pos.y -=TimeMgr.GetDt() * self.speed
         if GetKey(SDLK_d) == "TAP" or GetKey(SDLK_d) == "HOLD":
             self.pos.x +=TimeMgr.GetDt() * self.speed
+        if GetKey(SDLK_q) == "TAP" or GetKey(SDLK_q) == "HOLD":
+            self.ani.cur_ani = 2
+            self.ani_reset = True
+        if GetKey(SDLK_e) == "TAP" or GetKey(SDLK_e) == "HOLD":
+            self.ani.cur_ani = 3
+            self.ani_reset = True
 
-        if GetKey(SDLK_1) == "TAP":
-            self.cur_ani = 0
-            self.ani_reset = True
-        if GetKey(SDLK_2) == "TAP":
-            self.cur_ani = 1
-            self.ani_reset = True
-        if GetKey(SDLK_3) == "TAP":
-            self.ani_reset = True
-            self.cur_ani = 2
-        if GetKey(SDLK_4) == "TAP":
-            self.ani_reset = True
-            self.cur_ani = 3
-
-        self.ani.update(self.cur_ani,self.ani_reset)
-        self.ani_reset = False
+        self.ani.update()
 
 class Idle:
+
+    @staticmethod
+    def init(_instance):
+        _instance.ani.cur_ani = 0
+
     @staticmethod
     def update(_instance):
+        _instance.ani.update(False)
         pass
 
     @staticmethod
     def render(_instance):
+        _instance.ani.render(_instance.pos,_instance.dir)
         pass
 
 
