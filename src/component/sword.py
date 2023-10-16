988468401fdefce90f43ae9d313a82cb53bc90a4
jjuncode <jjun.code@gmail.com>
2023-10-16 15:24:54 +0900

    Attack Down set Collider by ani frame
    애니메이션 프레임에 맞춰서
    Attack Up Collider처리 완료

--- src/component/sword.py
+++ src/component/sword.py
@@ -1,18 +1,20 @@
 from src.component.Collider import Collider
 from src.struct.struct import Vec2
 from src.component.Component import Component
-from src.component.StateMachine import Idle,Run,Attack_down,Attack_up
+from src.component.StateMachine import Idle, Run, Attack_down, Attack_up
 from src.mgr.TimeMgr import TimeMgr
 
-origin_collider_size = Vec2(10,10)
+origin_collider_size = Vec2(10, 10)
+
 
 class Sword(Component):
-    def __init__(self,_owner,_pos):
+    def __init__(self, _owner, _pos):
         super().__init__(_owner, _pos)
 
         # < Component >
-        self.component= {}
-        self.collider = Collider(self,Vec2(self.pos.x,self.pos.y),Vec2(origin_collider_size.x,origin_collider_size.y))
+        self.component = {}
+        self.collider = Collider(self, Vec2(self.pos.x, self.pos.y),
+                                 Vec2(origin_collider_size.x, origin_collider_size.y))
         self.component["COLLIDER"] = self.collider
 
     def update(self):
@@ -23,19 +25,21 @@ class Sword(Component):
 
         elif self.owner.getCurState() == Attack_up:
             self.pos.x = self.owner.getPos().x + 25 * self.owner.ani.cur_frame
-            self.component["COLLIDER"].scale.x = 10 + 10 * (self.owner.ani.cur_frame -1 )
-            self.component["COLLIDER"].scale.y = 10 + 22 * (self.owner.ani.cur_frame -1)
+            self.component["COLLIDER"].scale.x = 10 + 10 * (self.owner.ani.cur_frame - 1)
+            self.component["COLLIDER"].scale.y = 10 + 22 * (self.owner.ani.cur_frame - 1)
 
 
         elif self.owner.getCurState() == Attack_down:
-            pass
+            self.pos.x = self.owner.getPos().x + 25 * self.owner.ani.cur_frame
+            self.component["COLLIDER"].scale.x = 10 + 15 * (self.owner.ani.cur_frame - 1)
+            self.component["COLLIDER"].scale.y = 10 + 15 * (self.owner.ani.cur_frame - 1)
 
         for key, value in self.component.items():
             value.update()
         pass
 
     def render(self):
-        for key,value in self.component.items():
+        for key, value in self.component.items():
             value.render()
 
     def getPos(self):
