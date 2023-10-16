c3ef22b0781306064e903193c0d3798e93cd2cf0
jjuncode <jjun.code@gmail.com>
2023-10-16 15:15:45 +0900

    Attack Up set Collider by ani frame
    애니메이션 프레임에 맞춰서
    Attack Up Collider처리 완료

--- src/component/sword.py
+++ src/component/sword.py
@@ -4,20 +4,28 @@ from src.component.Component import Component
 from src.component.StateMachine import Idle,Run,Attack_down,Attack_up
 from src.mgr.TimeMgr import TimeMgr
 
+origin_collider_size = Vec2(10,10)
+
 class Sword(Component):
     def __init__(self,_owner,_pos):
         super().__init__(_owner, _pos)
 
         # < Component >
         self.component= {}
-        self.collider = Collider(self,Vec2(self.pos.x,self.pos.y),Vec2(50,10))
+        self.collider = Collider(self,Vec2(self.pos.x,self.pos.y),Vec2(origin_collider_size.x,origin_collider_size.y))
         self.component["COLLIDER"] = self.collider
 
     def update(self):
         if self.owner.getCurState() == Idle or self.owner.getCurState() == Run:
             self.pos = self.owner.getPos()
+            self.component["COLLIDER"].scale.x = origin_collider_size.x
+            self.component["COLLIDER"].scale.y = origin_collider_size.y
+
         elif self.owner.getCurState() == Attack_up:
-            self.pos.x = self.owner.getPos().x + 10 * self.owner.ani.cur_frame
+            self.pos.x = self.owner.getPos().x + 25 * self.owner.ani.cur_frame
+            self.component["COLLIDER"].scale.x = 10 + 10 * (self.owner.ani.cur_frame -1 )
+            self.component["COLLIDER"].scale.y = 10 + 22 * (self.owner.ani.cur_frame -1)
+
 
         elif self.owner.getCurState() == Attack_down:
             pass
