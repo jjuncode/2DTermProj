b231a155a88a7244f749b534f3ecf662288e023d
jjuncode <jjun.code@gmail.com>
2023-11-06 02:01:07 +0900

    Alpha Blending
    using opacify(alpha value)

--- src/object/title_background.py
+++ src/object/title_background.py
@@ -1,12 +1,12 @@
 from src.struct.struct import Vec2
 from pico2d import load_image
 from src.component.ani import CreatePath
+import pico2d
 
 class TitleBackground:
     def __init__(self):
         self.pos = Vec2(400,300)
         self.cur_image = load_image(CreatePath('title_background.png'))
-
 
     def render(self):
         self.cur_image.draw(self.pos.x,self.pos.y,800,600)
