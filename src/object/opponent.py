e2418382d38e728ebfbd3d2c5c920787183ae1c8
jjuncode <jjun.code@gmail.com>
2023-11-19 01:35:58 +0900

    Create AI

--- src/object/opponent.py
+++ src/object/opponent.py
@@ -122,18 +122,26 @@ class Opponent:
         c_hp_more = Condition("플레이어보다 체력이 많거나 같은가", self.is_more_hp)
         c_hp_less = Condition("플레이어보다 체력이 적은가", self.is_less_hp)
 
-        c_distn = Condition("일정거리 이내", self.is_near_player,300)
+        c_distn = Condition("일정거리 이내", self.is_near_player,200)
+        c_is_attack = Condition("상대가 공격함", self.is_player_attack)
+        c_is_idle = Condition("상대가 Idle", self.is_player_idle)
 
         a_move_forward = Action('전진', self.move_forward)  # action node 생성
         a_move_back = Action("후진", self.move_back)
-        a_attakc_down = Action("공격",self.attack_down)
 
-        SEQ_move_froward = Sequence("앞으로 이동",c_hp_more,a_move_forward)
+        a_attack_down = Action("공격",self.attack_down)
+        a_attack_parrying = Action("패링", self.attack_parrying)
+
+        SEQ_move_by_idle= Sequence("앞으로 이동_by HP",c_hp_more,a_move_forward)
+        SEQ_move_by_hp= Sequence("앞으로 이동_by IDLE",c_is_idle,a_move_forward)
+        SEQ_move_froward = Selector("앞으로 이동",SEQ_move_by_hp,SEQ_move_by_idle)
+
         SEQ_move_back = Sequence("뒤로 이동",c_hp_less,a_move_back)
 
-        SEQ_attack = Sequence("공격", c_distn,a_attakc_down)
+        SEQ_attack = Sequence("공격", c_distn,a_attack_down)
+        SEQ_parrying = Sequence("패링", c_distn,c_is_attack,a_attack_parrying)
 
-        root = Selector("이동/후퇴", SEQ_attack,SEQ_move_froward,SEQ_move_back)
+        root = Selector("패링/공격/이동/후퇴", SEQ_parrying,SEQ_attack,SEQ_move_froward,SEQ_move_back)
 
         self.bt = BehaviorTree(root)
         self.component["BT"] = self.bt
@@ -162,6 +170,21 @@ class Opponent:
         if self.pos.x - player[0].getPos().x < _distn : return BehaviorTree.SUCCESS
         else : return BehaviorTree.FAIL
 
+    def is_player_attack(self):
+        from src.mgr.SceneMgr import SceneMgr
+        player = SceneMgr.getCurScene().getObj(OBJ.kPlayer)
+        if player[0].getCurState() == Attack_down or player[0].getCurState()==Attack_up :
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.FAIL
+
+    def is_player_idle(self):
+        from src.mgr.SceneMgr import SceneMgr
+        player = SceneMgr.getCurScene().getObj(OBJ.kPlayer)
+        if player[0].getCurState() == Idle :
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.FAIL
     def move_forward(self):
         if self.getCurState() != Front:
             self.state.change_state("FRONT")
@@ -170,5 +193,9 @@ class Opponent:
             self.state.change_state("BACK")
 
     def attack_down(self):
+        if self.getCurState() != Attack_down_Opp:
+            self.state.change_state("ATTACK_DOWN")
+
+    def attack_parrying(self):
         if self.getCurState() != Attack_up_Opp:
             self.state.change_state("PARRYING")
